fa204e149298bfdf17afaa4167aa1081e0ff54c6
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-15 12:50:22 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,18 +1,21 @@
 import { REST, Routes, ActivityType } from "discord.js";
 
+const CLIENT_ID = process.env.CLIENTID.replace(/"/g, '');
+const TOKEN = process.env.TOKEN.replace(/"/g, '');
+
 export default {
   name: "ready",
   once: true,
   async execute(client) {
     console.log(`Logged in as ${client.user.tag}`);
 
-    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+    const rest = new REST({ version: "10" }).setToken(TOKEN);
 
     const commands = client.commands.map(cmd => cmd.data.toJSON());
 
     try {
       await rest.put(
-        Routes.applicationCommands(process.env.CLIENTID),
+        Routes.applicationCommands(CLIENT_ID),
         { body: commands }
       );
       console.log("Global slash commands registered successfully.");
