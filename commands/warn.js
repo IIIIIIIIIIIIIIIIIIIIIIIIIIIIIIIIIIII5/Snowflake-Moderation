41a515e6e75bb6319d23d746a67a4d77e7528572
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-15 09:10:05 -0700

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { logAction } from '../utils/logAction.js';
+
+const ALLOWED_ROLES = ['1398691449939169331', '1386369108408406096'];
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Warn a user.')
+        .addUserOption(option =>
+            option.setName('target').setDescription('User to warn').setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason').setDescription('Reason for warning').setRequired(true)),
+
+    async execute(interaction) {
+        if (!interaction.member.roles.cache.some(r => ALLOWED_ROLES.includes(r.id)))
+            return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+
+        const member = interaction.options.getMember('target');
+        const reason = interaction.options.getString('reason');
+        if (!member) return interaction.reply({ content: 'User not found.', ephemeral: true });
+
+        const embed = new EmbedBuilder()
+            .setDescription(`<@${member.id}> has been warned. Reason: ${reason}`)
+            .setColor(0xFFD700)
+            .setTimestamp()
+            .setFooter({ text: `Warned by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+        await interaction.reply({ embeds: [embed] });
+
+        await logAction({
+            type: 'warn',
+            user: member.id,
+            moderator: interaction.user.id,
+            reason
+        });
+    }
+};
