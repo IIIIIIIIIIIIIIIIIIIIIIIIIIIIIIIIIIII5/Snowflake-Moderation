3cebd700079da7aedd0b6c28c550526731ef9c1e
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-12-09 01:36:37 -0500

    Update fakeban.js

--- commands/fakeban.js
+++ commands/fakeban.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
-const AllowedRoles = ['1398691449939169331','1386369108408406096','1418979785165766717'];
+const AllowedRoles = ['1398691449939169331','1386369108408406096','1418979785165766717', '1443622126203572304'];
 
 export default {
   data: new SlashCommandBuilder()
