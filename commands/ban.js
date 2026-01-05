25881b6693c99c6e6e18cf522a2cb0ed7d2f2857
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2026-01-05 11:15:46 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,16 +1,28 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 import { logAction } from '../utils/logAction.js';
 
-const AllowedRoles = ['1398691449939169331','1386369108408406096','1418979785165766717', '1443622126203572304'];
+const AllowedRoles = [
+  '1398691449939169331',
+  '1386369108408406096',
+  '1418979785165766717',
+  '1443622126203572304'
+];
+
 const LogChannelId = '1419190262697033758';
 
 export default {
   data: new SlashCommandBuilder()
     .setName('ban')
     .setDescription('Ban a user from the server.')
-    .addUserOption(option => option.setName('target').setDescription('User to ban').setRequired(true))
-    .addStringOption(option => 
-      option.setName('appealable')
+    .addUserOption(option =>
+      option
+        .setName('target')
+        .setDescription('User to ban')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('appealable')
         .setDescription('Can this punishment be appealed?')
         .setRequired(true)
         .addChoices(
@@ -18,22 +30,38 @@ export default {
           { name: 'No', value: 'No' }
         )
     )
-    .addStringOption(option => option.setName('reason').setDescription('Reason for the ban').setRequired(false)),
+    .addStringOption(option =>
+      option
+        .setName('reason')
+        .setDescription('Reason for the ban')
+        .setRequired(false)
+    ),
 
   async execute(Interaction) {
-    if(!Interaction.member.roles.cache.some(r => AllowedRoles.includes(r.id)))
-      return Interaction.reply({ content:'You do not have permission.', ephemeral:true });
+    await Interaction.deferReply();
+
+    if (!Interaction.member.roles.cache.some(r => AllowedRoles.includes(r.id))) {
+      return Interaction.editReply({
+        content: 'You do not have permission.'
+      });
+    }
 
     const TargetUser = Interaction.options.getUser('target');
     const TargetMember = Interaction.options.getMember('target');
-    const Appealable = Interaction.options.getString('appealable') || 'Yes';
-    const Reason = Interaction.options.getString('reason') || 'No reason provided.';
+    const Appealable = Interaction.options.getString('appealable') ?? 'Yes';
+    const Reason = Interaction.options.getString('reason') ?? 'No reason provided.';
 
-    if(TargetMember && !TargetMember.bannable)
-      return Interaction.reply({ content:'I cannot ban this user.', ephemeral:true });
+    if (TargetMember && !TargetMember.bannable) {
+      return Interaction.editReply({
+        content: 'I cannot ban this user.'
+      });
+    }
 
-    let DmDescription = `You have been banned from Snowflake Penitentiary Communications Server, for **${Reason}**.`;
-    if (Appealable === 'Yes') DmDescription += `\n\nIf you feel this punishment has been delivered to you unfairly then join our [Administration Server](https://discord.gg/ZSJuzdVAee) to appeal your punishment.`;
+    let DmDescription = `You have been banned from Snowflake Penitentiary Communications Server for **${Reason}**.`;
+
+    if (Appealable === 'Yes') {
+      DmDescription += `\n\nIf you believe this punishment was unfair, join our [Administration Server](https://discord.gg/ZSJuzdVAee) to appeal.`;
+    }
 
     const DmEmbed = new EmbedBuilder()
       .setTitle('PUNISHMENT RECEIVED')
@@ -41,33 +69,58 @@ export default {
       .setColor(0xFF0000)
       .setTimestamp();
 
-    try { await TargetUser.send({ embeds:[DmEmbed] }); } catch {}
+    try {
+      await TargetUser.send({ embeds: [DmEmbed] });
+    } catch {}
 
     await Interaction.guild.members.ban(TargetUser.id, { reason: Reason });
 
-    const rawLogId = await logAction({ type:'ban', user:TargetUser.id, moderator:Interaction.user.id, reason: Reason, appealable: Appealable });
-    const PunishmentId = rawLogId || '#N/A';
+    let PunishmentId = '#N/A';
+    try {
+      const rawLogId = await logAction({
+        type: 'ban',
+        user: TargetUser.id,
+        moderator: Interaction.user.id,
+        reason: Reason,
+        appealable: Appealable
+      });
+      PunishmentId = rawLogId ?? '#N/A';
+    } catch (err) {
+      console.error('Logging failed:', err);
+    }
 
     const PublicEmbed = new EmbedBuilder()
-      .setDescription(`<@${TargetUser.id}> has been banned. Reason: ${Reason}`)
+      .setDescription(`<@${TargetUser.id}> has been banned.\n**Reason:** ${Reason}`)
       .setColor(0xFF0000)
-      .setTimestamp()
-      .setFooter({ text:`Banned by ${Interaction.user.tag}` });
+      .setFooter({ text: `Banned by ${Interaction.user.tag}` })
+      .setTimestamp();
 
-    await Interaction.reply({ embeds:[PublicEmbed] });
+    await Interaction.editReply({ embeds: [PublicEmbed] });
 
-    const LogEmbed = new EmbedBuilder()
-      .setTitle(`Ban Issued - ${TargetUser.tag}`)
-      .addFields(
-        { name:'Reason', value:Reason },
-        { name:'Appealable', value:Appealable },
-        { name:'Issued by', value:`<@${Interaction.user.id}>` },
-        { name:'Time', value:new Date().toLocaleString('en-US',{ dateStyle:'full', timeStyle:'short' }) }
-      )
-      .setFooter({ text:`Case ${PunishmentId}` })
-      .setColor(0xFF0000);
+    try {
+      const LogChannel = await Interaction.client.channels.fetch(LogChannelId);
+      if (!LogChannel) return;
 
-    const LogChannel = await Interaction.client.channels.fetch(LogChannelId);
-    await LogChannel.send({ embeds:[LogEmbed] });
+      const LogEmbed = new EmbedBuilder()
+        .setTitle(`Ban Issued - ${TargetUser.tag}`)
+        .addFields(
+          { name: 'Reason', value: Reason },
+          { name: 'Appealable', value: Appealable },
+          { name: 'Issued by', value: `<@${Interaction.user.id}>` },
+          {
+            name: 'Time',
+            value: new Date().toLocaleString('en-US', {
+              dateStyle: 'full',
+              timeStyle: 'short'
+            })
+          }
+        )
+        .setFooter({ text: `Case ${PunishmentId}` })
+        .setColor(0xFF0000);
+
+      await LogChannel.send({ embeds: [LogEmbed] });
+    } catch (err) {
+      console.error('Failed to send log:', err);
+    }
   }
 };
